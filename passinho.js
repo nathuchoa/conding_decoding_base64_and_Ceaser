7e3bda96babdd9d6e5bcc921718510904b7fc349
nathuchoa <nathaliauchoa@hotmail.com>
2022-04-29 21:03:47 -0300

    alteações no chamamento da função

--- passinho.js
+++ passinho.js
@@ -1,39 +1,32 @@
 var entradaTexto = document.getElementById("texto");
 var selecionandoCifra = document.getElementById("selecao");
 var adicionandoIncremento = document.getElementById("incremento");
-
-function tipoDeCifra() {
-  selecionandoCifra.addEventListener("click", function () {
-    if (selecionandoCifra.value == "cifraDeCesar") {
-      adicionandoIncremento.style.display = "flex";
-      adicionandoIncremento.innerHTML = `<label for="incremento">Qual o incremento?</label>
-      <input type="number" name="indique o incremento" id="incrementos" required>`;
-    } else if (selecionandoCifra.value == "base64") {
-      adicionandoIncremento.style.display = "none";
-    }
-  });
-}
-tipoDeCifra();
-
-// SELECIONANDO A AÇÃO
-var varEscolhendoSaida = document.getElementById("escolhendoSaida");
 var escolhendoCodificar = document.getElementById("codificar");
 var escolhendoDecodificar = document.getElementById("decodificar");
 var divBotaoEnvio = document.getElementById("envio");
 var botaoCodificar = document.getElementById("btn-codificar");
 var botaoDecodificar = document.getElementById("btn-decodificar");
 
-function escolhendoSaida() {
-  escolhendoDecodificar.addEventListener("click", function () {
-    if (true) {
-      divBotaoEnvio.innerHTML = `<input type="submit" value="Decodificar Mensagem" id="btn-decodificar">`;
-    }
-  });
-  escolhendoCodificar.addEventListener('click', function(){
-    if (true){
-      botaoCodificar.style.display = 'flex';
-      divBotaoEnvio.innerHTML = `<input type="submit" value="Codificar Mensagem" id="btn-codificar">`;
-    }
-  });
-}
-escolhendoSaida();
+selecionandoCifra.addEventListener("click", function () {
+  if (selecionandoCifra.value == "cifraDeCesar") {
+    adicionandoIncremento.style.display = "flex";
+    adicionandoIncremento.innerHTML = `<label for="incremento">Qual o incremento?</label>
+      <input type="number" name="indique o incremento" id="incrementos" min="-25" max="25" required>`;
+  } else if (selecionandoCifra.value == "base64") {
+    adicionandoIncremento.style.display = "none";
+  }
+});
+
+// SELECIONANDO A AÇÃO
+
+escolhendoDecodificar.addEventListener("click", function () {
+  if (true) {
+    divBotaoEnvio.innerHTML = `<input type="submit" value="Decodificar Mensagem" id="btn-decodificar">`;
+  }
+});
+escolhendoCodificar.addEventListener("click", function () {
+  if (true) {
+    divBotaoEnvio.innerHTML = `<input type="submit" value="Codificar Mensagem" id="btn-codificar">`;
+  }
+});
+
