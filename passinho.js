8febc15d665285aa0a48ea9a66c24935063b3cda
nathuchoa <nathaliauchoa@hotmail.com>
2022-05-05 23:45:44 -0300

    Codificando e Decodificando

--- passinho.js
+++ passinho.js
@@ -1,40 +1,112 @@
 var entradaTexto = document.getElementById("texto");
-var selecionandoCifra = document.getElementById("selecao");
-var adicionandoIncremento = document.getElementById("incremento");
+var selecao = document.getElementById("selecao");
+var passos = document.getElementById("incremento");
 var escolhendoCodificar = document.getElementById("codificar");
 var escolhendoDecodificar = document.getElementById("decodificar");
 var divBotaoEnvio = document.getElementById("envio");
-var botaoCodificar = document.getElementById("btn-codificar");
-var botaoDecodificar = document.getElementById("btn-decodificar");
 
-selecionandoCifra.addEventListener("click", function () {
-  if (selecionandoCifra.value == "cifraDeCesar") {
-    adicionandoIncremento.style.display = "flex";
-    adicionandoIncremento.innerHTML = `<label for="incremento">Qual o incremento?</label>
-      <input type="number" name="indique o incremento" id="incrementos" min="-25" max="25" required>`;
-  } else if (selecionandoCifra.value == "base64") {
-    adicionandoIncremento.style.display = "none";
+selecao.addEventListener("click", function () {
+  if (selecao.value == "cifraDeCesar") {
+    passos.style.display = "flex";
+    passos.innerHTML = `<label for="incremento">Qual o incremento?</label>
+      <input type="number" name="indique o incremento" id="incrementos" min="-25" max="25">`;
+  } else if (selecao.value == "base64") {
+    passos.style.display = "none";
   }
 });
-
-// SELECIONANDO A AÇÃO
 
 escolhendoDecodificar.addEventListener("click", function () {
   if (true) {
     divBotaoEnvio.innerHTML = `<input type="submit" value="Decodificar Mensagem" id="btn-decodificar">`;
+    document.getElementById("btn-decodificar").addEventListener("click", decodificando);
+    var prevent = document.querySelector('#btn-decodificar')
+    prevent.addEventListener("click", (e) => {e.preventDefault()});
   }
 });
 
 escolhendoCodificar.addEventListener("click", function () {
   if (true) {
     divBotaoEnvio.innerHTML = `<input type="submit" value="Codificar Mensagem" id="btn-codificar">`;
+    document.getElementById("btn-codificar").addEventListener("click", codificando);
+    var prevent = document.querySelector('#btn-codificar')
+    prevent.addEventListener("click", (e) => {e.preventDefault()});
   }
 });
 
-/* CAMPO DA RESPOSTA */
+/* CODIFICANDO E DECODIFICANDO */
 
-/* CIFRA DE CESAR */
+var saidaTexto = document.querySelector("#resultado");
 
-/*  ((codigoASC - 65 +desloc)% 26) + 65
-    function cifraDeCesar(string){return ((codigoASC - 65 +desloc)% 26) + 65 } */
+function codificando() {
+  var recebeTexto = document.getElementById("texto").value;
+  if (selecao.value == "base64" && escolhendoCodificar.checked) {
+    txtValue = recebeTexto;
+    saidaTexto.value = btoa(txtValue);
+  } else if (selecao.value == "cifraDeCesar" && escolhendoCodificar.checked) {
+    codificandoCesar();
+  }
+}
 
+function codificandoCesar() {
+  var recebeTexto = document.getElementById("texto").value;
+  var adicionandoIncremento = document.getElementById("incremento").value;
+  var txtValue = "";
+
+  for (var i = 0; i < recebeTexto.length; i++) {
+    var chamandoIncremento = parseInt(adicionandoIncremento);
+    var ascii = recebeTexto[i].charCodeAt();
+    if (ascii >= 65 && ascii <= 90) {
+      var passo = ascii + chamandoIncremento;
+      if (passo > 90) {
+        passo = passo - 26;
+      }
+      txtValue += String.fromCharCode(passo);
+    } else if (ascii >= 97 && ascii <= 122) {
+      var passo = ascii + passo;
+      if (passo > 122) {
+        passo = passo - 26;
+      }
+      txtValue += String.fromCharCode(passo);
+    } else {
+      txtValue += recebeTexto[i];
+    }
+  }
+  saidaTexto.value = txtValue;
+}
+
+function decodificando() {
+  var recebeTexto = document.getElementById("texto").value;
+  if (selecao.value == "base64" && escolhendoDecodificar.checked) {
+    txtValue = recebeTexto;
+    saidaTexto.value = atob(txtValue);
+  } else if (selecao.value == "cifraDeCesar" && escolhendoDecodificar.checked) {
+    decodificandoCesar();
+  }
+}
+
+function decodificandoCesar() {
+  var recebeTexto = document.getElementById("texto").value;
+  var adicionandoIncremento = document.getElementById("incremento").value;
+  var txtValue = "";
+
+  for (var i = 0; i < recebeTexto.length; i++) {
+    var passo = parseInt(adicionandoIncremento);
+    var ascii = recebeTexto[i].charCodeAt();
+    if (ascii >= 65 && ascii <= 90) {
+      var passo = ascii - passo;
+      if (passo < 65) {
+        passo = 26 + passo;
+      }
+      txtValue += String.fromCharCode(passo);
+    } else if (ascii >= 97 && ascii <= 122) {
+      var passo = ascii - passo;
+      if (passo < 97) {
+        passo = 26 + passo;
+      }
+      txtValue += String.fromCharCode(passo);
+    } else {
+      txtValue += recebeTexto[i];
+    }
+  }
+  saidaTexto.value = txtValue;
+}
